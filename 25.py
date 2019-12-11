abec4d495a7095096e3d97ec8fddd0ef202cb42a
Shelzi99 <criv.shelley@gmail.com>
2019-12-11 20:22:09 +0200

    Changed boys/girls issue

--- 25.py
+++ 25.py
@@ -21,7 +21,7 @@ def main():
 
     # Creating two dictionaries: (1) country and the no. of births in each state between the years 1986-2006
     # (2) No. of boys and girtls in each country:
-    def accumulate_births_per_state():
+    def accumilate_births_per_state():
         with open('births1.xls', 'r') as data_dict:
             # Ignore first line in the file since it has headers - by reading the the first line and after starting the 'for' loop:
             data_dict.readline()
@@ -47,40 +47,35 @@ def main():
                     else:
                         birth_in_each_state[temp_state] = temp_births
 
-                # Function for creating dictionary boys and girls
-                if check_if_state_in_dict2():
-                    if sex == '"boy"':
-                        # Accumulating the no. of boy births into a temporary variable (temp_births_boys)
-                        # by adding the current no. of boy-births to the no. already
-                        # in the dictinary (of that specific state):
+                # Creating dictionary II - including each country and no. of boys and girls:
+                if sex == '"boy"':
+                    if check_if_state_in_dict2():
+                        # Accumulating  no. of 'boy' births in that country:
                         temp_births_boy = temp_births + boys_girls_in_each_state.get(temp_state)[0]
-                        boys_girls_in_each_state[temp_state] = [temp_births_boy, boys_girls_in_each_state.get(temp_state)[1]]
-
-                    elif sex == '"girl"':
+                        boys_girls_in_each_state[temp_state] = [temp_births_boy, temp_births_girl]
+                    else:
+                        boys_girls_in_each_state[temp_state] = [temp_births, temp_births_girl]
+                elif sex == '"girl"':
+                    if check_if_state_in_dict2():
+                        # Accumulating  no. of 'boy' births in that country:
                         temp_births_girl = temp_births + boys_girls_in_each_state.get(temp_state)[1]
                         boys_girls_in_each_state[temp_state] = [boys_girls_in_each_state.get(temp_state)[0], temp_births_girl]
-
-                else:
-                    if fields[2] == '"boy"':
-                        boys_girls_in_each_state[temp_state] = [temp_births, temp_births_girl]
-
-                    if sex == '"girl"':
-                        boys_girls_in_each_state[temp_state] = [temp_births_boy, temp_births]
-                        print(boys_girls_in_each_state)
+                    else:
+                        birth_in_each_state[temp_state] = [temp_births, temp_births_girl]
 
     def print_message():
         print('Between the years 1981-2006:')
         for i in boys_girls_in_each_state:
-            no_of_boys = boys_girls_in_each_state.get(i)[0]
-            no_of_girls = boys_girls_in_each_state.get(i)[1]
-            if no_of_boys > no_of_girls:
-                print('In {} there were more boys ({:,}) than girls ({:,})'.format(i, no_of_boys, no_of_girls))
-            elif no_of_boys < no_of_girls:
-                print('In {} there were more girls ({}) than boys ({})'.format(i, no_of_girls, no_of_boys))
+            no_boys = boys_girls_in_each_state.get(i)[0]
+            no_girls = boys_girls_in_each_state.get(i)[1]
+            if no_boys > no_girls:
+                print('In {} there were more boys ({:,}) than girls ({:,})'.format(i, no_boys, no_girls))
+            elif no_boys < no_girls:
+                print('In {} there were more girls ({}) than boys ({})'.format(i, no_girls, no_boys))
             else:
-                print('In {} there were the same no. of girls ({}) and boys ({})'.format(i, no_of_girls, no_of_boys))
+                print('In {} there were the same no. of girls ({}) and boys ({})'.format(i, no_girls, no_boys))
 
-    accumulate_births_per_state()
+    accumilate_births_per_state()
 
     a = max(birth_in_each_state, key=birth_in_each_state.get)
     b = max(birth_in_each_state.values())
